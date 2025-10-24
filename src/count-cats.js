1895c94c30322e529ca7b3a0eefbf1911e20101b
vika oneil <oneilcode111@gmail.com>
2025-10-24 23:31:43 +1000

    feat: resolve cat task

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../lib');
+const { NotImplementedError } = require("../lib");
 
 /**
  * Given matrix where you have to find cats by ears "^^"
@@ -14,11 +14,11 @@ const { NotImplementedError } = require('../lib');
  * ]) => 3`
  *
  */
-function countCats(/* matrix */) {
-  // Remove line below and write your code here
-  throw new NotImplementedError('Not implemented');
+const searchElement = "^^";
+function countCats(matrix) {
+  return matrix.flat().filter((el) => el === searchElement).length;
 }
 
 module.exports = {
-  countCats
+  countCats,
 };
